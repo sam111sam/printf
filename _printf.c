4f3cae4a8c8d41ef4d331b7cbe998931c859ac93
sam111sam <samuelnega1@gmail.com>
2024-06-25 03:30:22 -0700

    using helper func

--- _printf.c
+++ _printf.c
@@ -7,51 +7,74 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0;
+	int i = 0, count = 0, r_count;
 	va_list args;
-	char *tmp;
-	char a;
+	va_list args_copy;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
+	va_copy(args_copy, args);
 
 	while (format[i])
 	{
 		switch (format[i])
 		{
 			case '%':
-				if (format[i + 1] == 'c')
+				r_count = check(args_copy, (format + i + 1));
+				if (r_count  > 0)
 				{
-					a = (char) va_arg(args, int);
-					count += write(1, &a, 1);
-					i++;
-				}
-				else if (format[i + 1] == '%')
-				{
-					a = '%';
-					count += write(1, &a, 1);
-					i++;
-				}
-				else if (format[i + 1] == 's')
-				{
-					tmp = va_arg(args, char *);
-					count += write(1, tmp, strlen(tmp));
+					count += r_count;
 					i++;
 				}
 				else
 				{
-					a = '%';
-					count += write(1, &a, 1);
+					return (-1);
 				}
 				break;
-
-
 			default:
-				count += write(1,&format[i], 1);
+				count += write(1, &format[i], 1);
 		}
 		i++;
 	}
 	return (count);
 }
+/**
+ * check - check the str specifier
+ * @args: args
+ * @cp: char pointer
+ * Return: int
+ */
+int check(va_list args, const char *cp)
+{
+	char *tmp, a;
+
+	if (*cp == 'c')
+	{
+		a = (char) va_arg(args, int);
+		return (write(1, &a, 1));
+	}
+	else if (*cp == '%')
+	{
+		a = '%';
+		return (write(1, &a, 1));
+	}
+	else if (*cp == 's')
+	{
+		tmp = va_arg(args, char *);
+		if (tmp)
+			return (write(1, tmp, strlen(tmp)));
+		else
+			return (write(1, "(null)", 6));
+	}
+	else
+	{
+		tmp = "Error";
+		write(2, &a, strlen(tmp));
+		return (-1);
+	}
+}
+
+
+
--- main.h
+++ main.h
@@ -4,4 +4,5 @@
 #include <stdarg.h>
 #include <string.h>
 int _printf(const char *format, ...);
+int check(va_list arg, const char *cp);
 #endif /*main_h */
